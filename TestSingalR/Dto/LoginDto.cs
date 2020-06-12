bdfaeb84d19d334448144f8c576e159adcf62e4a
math2018avan <math2018avan@gmail.com>
2020-06-12 20:07:53 +0800

    validation added

--- TestSingalR/Dto/LoginDto.cs
+++ TestSingalR/Dto/LoginDto.cs
@@ -1,8 +1,16 @@
-﻿namespace TestSingalR.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestSingalR.Dto
 {
     public class LoginDto
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- TestSingalR/Dto/RegisterDto.cs
+++ TestSingalR/Dto/RegisterDto.cs
@@ -1,10 +1,24 @@
-﻿namespace TestSingalR.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestSingalR.Dto
 {
     public class RegisterDto
     {
         public string DisplayName { get; set; }
         public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 }
